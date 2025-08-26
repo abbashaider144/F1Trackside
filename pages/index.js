c9814715b6b5b523244e82aec567edf949e1e046
abbashaider144 <supercoolbashu@gmail.com>
2025-08-26 16:24:21 -0400

    hello example (not important)

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function Standings() {
  return (
    <Layout>
      <div className="max-w-6xl mx-auto p-6">
-       <h1 className="text-4xl font-bold text-[#C91A1A] mb-6 text-center">F1 Official Standings</h1>
+       <h1 className="text-4xl font-bold text-[#C91A1A] mb-6 text-center">F1 Hello Standings</h1>
 
 
        <Tab.Group defaultIndex={0}>
